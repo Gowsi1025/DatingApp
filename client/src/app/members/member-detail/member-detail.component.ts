dcdf025e3e06db69de0dc9d4490b1b6b019facb1
Gowsi1025 <gowsihari789@gmail.com>
2024-08-15 16:50:48 +0530

    Updated to .net 8

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,52 +1,41 @@
 import { Component,  OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-member-detail',
     templateUrl: './member-detail.component.html',
     styleUrls: ['./member-detail.component.css'],
     standalone: true,
-    imports: [NgIf, TabsModule, NgxGalleryModule]
+    imports: [GalleryModule,TabsModule, CommonModule,]
 })
 export class MemberDetailComponent implements OnInit{
  member: Member | undefined;
- galleryOptions:NgxGalleryOptions[] = [];
- galleryImages:NgxGalleryImage[] = [];
+ images :GalleryItem[] = [];
+ 
+ 
+
  
   constructor(private memberService: MembersService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.loadMember();
 
-    this.galleryOptions = [
-      {
-        width:'500px',
-        height:'500px',
-        imagePercent:100,
-        thumbnailsColumns: 4,
-        imageAnimation:NgxGalleryAnimation.Slide,
-        preview:false
-      }
-    ]
+    this.loadMember();
+   
     
   }
 
   getImages(){
     if(!this.member) return [];
-    const imageUrls = [];
     for (const photo of this.member.photos) {
-      imageUrls.push({
-        small: photo.url,
-        medium: photo.url,
-        big: photo.url
-      })
+      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}))
     }
-    return imageUrls;
+    return this.images
+  
   }
 
   loadMember(): void {
@@ -55,7 +44,7 @@ export class MemberDetailComponent implements OnInit{
     this.memberService.getMember(username).subscribe({
       next: member =>{
         this.member = member;
-        this.galleryImages = this.getImages();
+        
       } 
     })
   }
